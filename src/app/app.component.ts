a906bde8602d6f91a4a7efec486c965bde89c91e
Yohan M <jhoanmendez06@gmail.com>
2025-07-28 22:01:55 -0500

    gg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 
 import { InputTextModule } from 'primeng/inputtext';
@@ -7,16 +7,35 @@ import { DialogModule } from 'primeng/dialog';
 import { CalendarModule } from 'primeng/calendar';
 import { DropdownModule } from 'primeng/dropdown';
 
+import { PrimeNG } from 'primeng/config';
+
+import { DatePickerModule } from 'primeng/datepicker';
+import { FormsModule } from '@angular/forms';
+import { FluidModule } from 'primeng/fluid';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [DialogModule, ButtonModule, InputTextModule, CalendarModule, DropdownModule],
+    imports: [DialogModule, ButtonModule, InputTextModule, CalendarModule, DropdownModule, DatePickerModule, FormsModule, FluidModule],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+constructor(private primeng: PrimeNG) {}
+
+    ngOnInit() {
+        this.primeng.ripple.set(true);
+        this.primeng.zIndex = {
+            modal: 1100,    // dialog, sidebar
+            overlay: 1000,  // dropdown, overlaypanel
+            menu: 1000,     // overlay menus
+            tooltip: 1100   // tooltip
+        };
+    }
+
     visible: boolean = false;
+
+      date1: Date | undefined;
 
     showDialog() {
         this.visible = true;
@@ -33,8 +52,16 @@ export class AppComponent {
         { label: 'Secundaria', value: 'secondary' },
         { label: 'Universidad', value: 'university' }
     ];
-    
+
     date2: Date | undefined;
+
+    relationshipOptions = [
+        { label: 'Soltero', value: 'single' },
+        { label: 'Casado', value: 'married' },
+        { label: 'Divorciado', value: 'divorced' },
+        { label: 'Viudo', value: 'widowed' }
+    ];
+
 
 }
 
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,9 +8,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import Lara from '@primeng/themes/lara';
+
+
+
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
     /*provideRouter(routes), provideAnimationsAsync(), providePrimeNG({theme: {preset: Aura}})*/
-  importProvidersFrom(BrowserAnimationsModule, BrowserModule)
+  importProvidersFrom(BrowserAnimationsModule, BrowserModule), 
+   provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                preset: Aura, // Puedes cambiar a Lara si lo prefieres
+                options: {
+                    prefix: 'p',
+                    darkModeSelector: 'false',
+                    cssLayer: false
+                }
+            }
+        })
   ]
 };
